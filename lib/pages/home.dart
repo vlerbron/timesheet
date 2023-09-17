45ee2a5d4f72eb281d130f438065eae01c05f6b4
vlerbron <vlerbron@hotmail.com>
2023-09-17 22:07:30 +0700

    Change display welcome date, Home Screen

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 import 'package:timesheet/utils/main_drawer.dart';
+
+final formatter = DateFormat('EEEE, dd MMMM yyyy');
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -10,7 +14,26 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Welcome to TBN PM"),
       ),
-      body: const Text('Home'),
+      body: Container(
+          margin: const EdgeInsets.all(8),
+          child: Row(
+            children: [
+              Text(
+                'It\'s ',
+                style: GoogleFonts.lato(
+                    color: const Color.fromARGB(255, 0, 0, 0),
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                formatter.format(DateTime.now()),
+                style: GoogleFonts.lato(
+                    color: const Color.fromARGB(255, 54, 25, 196),
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          )),
       drawer: const MainDrawer(),
     );
   }
