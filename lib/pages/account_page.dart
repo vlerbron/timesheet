1026ee226b197c01de687d112b8a6b911507ca44
vlerbron <vlerbron@hotmail.com>
2023-09-28 10:47:21 +0700

    Add Logout button

--- lib/pages/account_page.dart
+++ lib/pages/account_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:timesheet/widgets/tabs.dart';
 
@@ -10,6 +11,29 @@ class AccountPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('My Account'),
       ),
+      body: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+                Navigator.pushReplacementNamed(context, '/');
+              },
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 100,
+                  vertical: 15,
+                ),
+                backgroundColor: const Color.fromARGB(255, 30, 128, 184),
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: const Text('Log out'),
+            )
+          ]),
       bottomNavigationBar: const Tabs(selectedIndex: 4),
     );
   }
